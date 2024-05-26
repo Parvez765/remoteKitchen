986ad443f4caa17989247417d458e47f555e6523
ParvezHossainSakib <pervezhossain1998@gmail.com>
2024-05-26 09:34:03 +0600

    Food Data Added

---
+++ src/data/foodData.js
@@ -0,0 +1,11 @@
+import burger from "../assets/burger.jpg";
+
+export const foodData = [
+  {
+    id: 1,
+    img: burger,
+    name: "Burger",
+    description:
+      "A burger is a patty of ground beef grilled and placed between two halves of a bun. Slices of raw onion, lettuce, bacon, mayonnaise, and other ingredients add flavor. Burgers are considered an American food but are popular around the world. In Japan, teriyaki burgers are popular.",
+  },
+];
